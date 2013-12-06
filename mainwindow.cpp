2433c8e3b2693cc596414672f234f8bffb656bb0
feng-zhe <henryzhefeng@gmail.com>
2013-12-06 14:39:05 +0800

    Add the menubar, toolbar, statusbar

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -4,6 +4,7 @@
 #include <QMenuBar>
 #include <QToolBar>
 #include <QStatusBar>
+#include <QMessageBox>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -11,8 +12,23 @@ MainWindow::MainWindow(QWidget *parent) :
 {
     ui->setupUi(this);
     // now it's my code
-    startAction = new QAction(QIcon(":/image/tool-start"),tr("&Start"),this);
+    // set menubar, toolbar and statusbar
+    startAction = new QAction(QIcon(":/images/tool-start"),tr("&Start"),this);
+    startAction->setStatusTip(tr("Start a new SCanner"));
+    connect(startAction, &QAction::triggered, this, &MainWindow::start);
+    QMenu *menu = this->menuBar()->addMenu(tr("Settings"));
+    menu->addAction(startAction);
+    QToolBar *toolBar = this->addToolBar(tr("Shortcuts"));
+    toolBar->addAction(startAction);
+    this->statusBar();
 
+    // connect buttons and slots
+    connect(this->ui->buttonStart,&QPushButton::clicked,this,&MainWindow::start);
+}
+
+void MainWindow::start()
+{
+    QMessageBox::information(NULL, "Title", "licked!", QMessageBox::Yes | QMessageBox::No, QMessageBox::Yes);
 }
 
 MainWindow::~MainWindow()
--- mainwindow.h
+++ mainwindow.h
@@ -14,6 +14,9 @@ class MainWindow : public QMainWindow
 public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
+
+private:
+    void start();
     
 private:
     Ui::MainWindow *ui;
